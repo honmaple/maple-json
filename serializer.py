b3fe16d0ec44e6539647b439ebe5d079d2050499
jianglin <jianglin@1b2b.cn>
2017-01-19 17:21:16 +0800

    add PageInfo and Field

--- serializer.py
+++ serializer.py
@@ -6,44 +6,80 @@
 # Author: jianglin
 # Email: xiyang0807@gmail.com
 # Created: 2016-12-13 22:08:23 (CST)
-# Last Update:星期二 2016-12-13 22:8:57 (CST)
+# Last Update:星期四 2017-1-19 17:20:33 (CST)
 #          By:
 # Description:
 # **************************************************************************
 from sqlalchemy import inspect
 from sqlalchemy.orm.interfaces import (ONETOMANY, MANYTOMANY)
 
+__all__ = ['PageInfo', 'Field', 'Serializer']
+
+
+class PageInfo(object):
+    def __init__(self, paginate):
+        self.paginate = paginate
+
+    def as_dict(self):
+        pageinfo = {
+            'items': True,
+            'pages': self.paginate.pages,
+            'has_prev': self.paginate.has_prev,
+            'page': self.paginate.page,
+            'has_next': self.paginate.has_next,
+            'iter_pages': list(
+                self.paginate.iter_pages(
+                    left_edge=1, left_current=2, right_current=3,
+                    right_edge=1))
+        }
+        return pageinfo
+
+
+class Field(object):
+    def __init__(self, source, args={}, default=None):
+        self.source = source
+        self.args = args
+        self.default = default
+
+    def data(self, instance):
+        if hasattr(instance, self.source):
+            source = getattr(instance, self.source)
+            if not callable(source):
+                return source
+            return source(**self.args)
+        return self.default
+
 
 class Serializer(object):
-
-    def __init__(self, instance, many=False, include=[], exclude=[], depth=2):
+    def __init__(self,
+                 instance,
+                 many=False,
+                 include=[],
+                 exclude=[],
+                 extra=[],
+                 depth=0):
         self.instance = instance
         self.many = many
+        self.depth = depth
         self.include = include
         self.exclude = exclude
-        self.depth = depth
+        self.extra = extra
 
     @property
     def data(self):
+        meta = self.Meta
+        if not self.include and hasattr(meta, 'include'):
+            self.include = meta.include
+        if not self.exclude and hasattr(meta, 'exclude'):
+            self.exclude = meta.exclude
+        if not self.extra and hasattr(meta, 'extra'):
+            self.extra = meta.extra
+        if not self.depth:
+            self.depth = meta.depth if hasattr(meta, 'depth') else 2
         if self.include and self.exclude:
             raise ValueError('include and exclude can\'t work together')
         if self.many:
-            if isinstance(self.instance, list):
-                return self._serializerlist(self.instance, self.depth)
-            pageinfo = {
-                'items': True,
-                'pages': self.instance.pages,
-                'has_prev': self.instance.has_prev,
-                'page': self.instance.page,
-                'has_next': self.instance.has_next,
-                'iter_pages': list(self.instance.iter_pages(left_edge=1,
-                                                            left_current=2,
-                                                            right_current=3,
-                                                            right_edge=1))
-            }
-            return {'data': self._serializerlist(self.instance.items,
-                                                 self.depth),
-                    'pageinfo': pageinfo}
+            return self._serializerlist(self.instance, self.depth)
         return self._serializer(self.instance, self.depth)
 
     def _serializerlist(self, instances, depth):
@@ -63,8 +99,23 @@ class Serializer(object):
         inp = self.get_inspect(model_class)
         model_data = self._serializer_model(inp, instance, depth)
         relation_data = self._serializer_relation(inp, instance, depth)
+        extra_data = self._serializer_extra(instance)
         result.update(model_data)
         result.update(relation_data)
+        result.update(extra_data)
+        return result
+
+    def _serializer_extra(self, instance):
+        extra = self.extra
+        result = {}
+        for e in extra:
+            extra_column = getattr(self, e)
+            if isinstance(extra_column, Field):
+                result[e] = extra_column.data(instance)
+            else:
+                extra_column = getattr(instance, e)
+                result[e] = extra_column if not callable(
+                    extra_column) else extra_column()
         return result
 
     def _serializer_model(self, inp, instance, depth):
@@ -79,24 +130,31 @@ class Serializer(object):
         relation_columns = self.get_relation_columns(inp)
         for relation in relation_columns:
             column = relation.key
-            if relation.direction in [ONETOMANY, MANYTOMANY]:
+            if relation.direction in [ONETOMANY, MANYTOMANY
+                                      ] and relation.uselist:
                 children = getattr(instance, column)
                 if relation.lazy == 'dynamic':
                     children = children.all()
-                result[column] = Serializer(
-                    children,
-                    many=True,
-                    exclude=[relation.back_populates],
-                    depth=depth).data
+                if children:
+                    result[column] = Serializer(
+                        children,
+                        many=True,
+                        exclude=[relation.back_populates],
+                        depth=depth).data
+                else:
+                    result[column] = []
             else:
                 child = getattr(instance, column)
                 if relation.lazy == 'dynamic':
                     child = child.first()
-                result[column] = Serializer(
-                    child,
-                    many=False,
-                    exclude=[relation.back_populates],
-                    depth=depth).data
+                if child:
+                    result[column] = Serializer(
+                        child,
+                        many=False,
+                        exclude=[relation.back_populates],
+                        depth=depth).data
+                else:
+                    result[column] = {}
         return result
 
     def get_model_class(self, instance):
@@ -135,3 +193,9 @@ class Serializer(object):
         else:
             relation_columns = [relation for relation in inp.relationships]
         return relation_columns
+
+    class Meta:
+        depth = 2
+        include = []
+        exclude = []
+        extra = []
